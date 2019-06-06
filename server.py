628c3a16049494448771b2f301ce2dfa29823e1b
Aparna Karve <akarve@redhat.com>
2019-06-06 10:48:42 -0700

    Add GET index route for liveness/readiness checks

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 import logging
 import os
+import requests
 
 from flask import Flask, jsonify, request
 from flask.logging import default_handler
 from werkzeug.exceptions import BadRequest
+from urllib3.exceptions import NewConnectionError
+
+from workers import _retryable
 
 
 from workers import volume_type_validation_worker
@@ -22,6 +26,29 @@ APP = create_application()
 ROOT_LOGGER = logging.getLogger()
 ROOT_LOGGER.setLevel(APP.logger.level)
 ROOT_LOGGER.addHandler(default_handler)
+
+VERSION = "0.0.1"
+
+
+@APP.route("/", methods=['GET'])
+def get_root():
+    """Root Endpoint for Liveness/Readiness check."""
+    next_service = APP.config['NEXT_SERVICE_URL']
+    try:
+        _retryable('get', f'{next_service}')
+        status = 'OK'
+        message = 'Up and Running'
+        status_code = 200
+    except (requests.HTTPError, NewConnectionError):
+        status = 'Error'
+        message = 'aiops-publisher not operational'
+        status_code = 500
+
+    return jsonify(
+        status=status,
+        version=VERSION,
+        message=message
+    ), status_code
 
 
 @APP.route("/", methods=['POST', 'PUT'])
